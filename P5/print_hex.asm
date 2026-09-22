d46e7115ce056cafdbcafab0c0f8ffc9425dfbbb
arce-fernando <arce.fernando@uabc.edu.mx>
2026-09-22 20:43:38 +0000

    Commit 2

--- P5/print_hex.asm
+++ P5/print_hex.asm
@@ -6,25 +6,25 @@ section	.text
 
 _start:                     ;tell linker entry point
 
-	mov al,100
-	call pHex_b	; muestra en pantalla 64
+	mov al,10B
+	call pHex_b	; muestra en pantalla 64;  cambio a 02
 
 	mov al,10	; cambio de linea
 	call putchar
 
-	mov ax,0x12ab 	; presenta en pantalla 12AB
+	mov ax,0x64Eb 	; presenta en pantalla 12AB;  cambio a 64EB
 	call pHex_w
 
 	mov al,10 	; cambio de linea
 	call putchar
 
-	mov eax,1234ABCDh ; presenta en pantalla 1234ABCD
+	mov eax,567h ; presenta en pantalla 1234ABCD;  cambio a 00000567
 	call pHex_dw
 
 	mov al,0x0a	  ; cambio de lines 10 decimal o bien 0A hex
 	call putchar
 
-	mov eax,0x76543210 ; presenta en pantalla 76543210
+	mov eax,0xA67126AB ; presenta en pantalla 76543210;  cambio a A67126AB
         call pHex_dw
 
 	mov al,10       ; cambio de linea
